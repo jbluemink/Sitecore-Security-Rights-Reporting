aca00fb8ebbab35045c2a2ccb8da295bd3b42c08
Jan Bluemink <jan@mirabeau.nl>
2018-11-21 22:32:40 +0100

    Added the future, new start screen

---
+++ Security.Rights.Reporting/sitecore modules/Shell/Security-Rights-Reporting/UserListScreen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sitecore.Data;
+using System.Web.UI.WebControls;
+using Sitecore.Data.Items;
+using Security.Rights.Reporting.Shell;
+using Security.Rights.Reporting.Shell.RightsData;
+using Sitecore.Security.Accounts;
+
+namespace Security.Rights.Reporting.sitecore_modules.Shell.Security_Rights_Reporting
+{
+    public static class UserListScreen
+    {
+        public static void DisplayAccountRight(Literal userrights, Database db, string account, bool showdefaultrights)
+        {
+
+        }
+    }
+}
